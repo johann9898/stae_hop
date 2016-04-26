2b80dc6ca2c3ed6ff96176b9acf0fc41df3a2b9a
Jóhann Rúnarsson <johann9898@gmail.com>
2016-04-26 23:39:57 +0000

    Update seems like it working a bit but not perfectly

--- stae_hop/stae_hop/Program.cs
+++ stae_hop/stae_hop/Program.cs
@@ -10,12 +10,19 @@ namespace stae_hop
     {
         static void Main(string[] args)
         {
+           
+            Aðferðir adferd = new Aðferðir();
             double tala1 = 0;
             double tala2 = 0;
             double tala3 = 0;
             double tala4 = 0;
-
+            double positive1 = 0;
+            double positive2 = 0;
             double answer = 0;
+            double answer2 = 0;
+            double answer3 = 0;
+            
+            
             int val = 0;
             
 
@@ -38,21 +45,47 @@ namespace stae_hop
                         Console.Write("Veldu Tölu við Y-ás: ");
                         tala2 = Convert.ToDouble(Console.ReadLine());
 
-                        answer = Math.Atan(tala1 / tala2) * 180 / Math.PI; //Ekki rétt aðferð
 
-                        Console.WriteLine("Stefnuhorn Vígurinn er " + answer);
+                        Vigur v2 = new Vigur(tala1, tala2);
+                        Console.WriteLine();
+
+                        Console.WriteLine("Stefnuhorn Vígurinn er " + adferd.Stefnuhorn(v2));
                         Console.ReadKey();
                         break;
 
                     case 2:
                         Console.Clear();
-                        Console.Write("Veldu Tölu við X-ás: ");
+                        Console.Write("Veldu Tölu við X-ás Fyrstu Stefnuhorn: ");
                         tala1 = Convert.ToDouble(Console.ReadLine());
                         Console.WriteLine();
-                        Console.Write("Veldu Tölu við Y-ás: ");
+                        Console.Write("Veldu Tölu við Y-ás Fyrstu Stefnuhorn: ");
                         tala2 = Convert.ToDouble(Console.ReadLine());
-                        answer = Math.Atan(tala1 / tala2) * 180 / Math.PI;
-                        Console.WriteLine("Stefnuhorn Vígurinn er " + answer);
+                        Console.WriteLine();
+                        Console.Write("Veldu Tölu við X-ás Annari Stefnuhorn: ");
+                        tala3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Veldu Tölu við Y-ás Annari Stefnuhorn: ");
+                        tala4 = Convert.ToDouble(Console.ReadLine());
+                        positive1 = Math.Atan(tala2 / tala1) * 180 / Math.PI;
+                        positive2 = Math.Atan(tala4 / tala3) * 180 / Math.PI;
+
+                        answer = Math.Abs(positive1);
+                        answer2 = Math.Abs(positive2);
+
+                        answer3 = answer + answer2;
+
+                       
+
+                        if(positive1 < positive2)
+                        {
+                            answer3 = answer3 * -1;
+                        }
+                        if (positive1 > positive2)
+                        {
+                            answer3 = answer3 * 1;
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Stefnuhorn Vígurinn er " + answer3);
                         Console.ReadKey();
                         break;
                     case 3:
@@ -91,10 +124,10 @@ namespace stae_hop
                         Vigur v1 = new Vigur(5,8);
                         Console.WriteLine(adferdir.Stefnuhorn(v1));
 
-                        Vigur v2 = new Vigur(3,-9);
-                        Console.WriteLine(adferdir.Stefnuhorn(v2));
+                        Vigur v3 = new Vigur(3,9);
+                        Console.WriteLine(adferdir.Stefnuhorn(v3));
 
-                        Console.WriteLine(adferdir.Stefnuhorn(v2) - adferdir.Stefnuhorn(v1));
+                        Console.WriteLine(adferdir.ToDeg(adferdir.Stefnuhorn(v3) - adferdir.Stefnuhorn(v1)));
                         Console.ReadKey();
                         break;
                 }
